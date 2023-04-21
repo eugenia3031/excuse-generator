9ceebc3e0a9e5ae319942eff612c28ac924dc13f
eugenia rivero <eugeniarivero3031@gmail.com>
2023-04-21 22:39:12 +0000

    final excuse

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#reload").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generate();
+  });
+};
+
+let generate = () => {
+  let who = ["My dog", "The police", "A tornado", "My cat", "My grandpa"];
+  let what = ["ate", "took", "flew", "float", "stole"];
+  let which = ["my homework", "my car", "my house", "my chair", "my cellphone"];
+  let when = ["yesterday.", "this morning.", "last night.", "last week."];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whichIndex = Math.floor(Math.random() * which.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    which[whichIndex] +
+    " " +
+    when[whenIndex]
+  );
 };
